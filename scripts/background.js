d1f35562ada374fc03fc19de572a2ed4c21cecca
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2023-06-25 23:54:04 +0900

    fix: add black filter over backgroundimg

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,6 @@
 const imgNum = 6
 const selected = Math.floor(Math.random()*imgNum)
 console.log("imgNum: ", selected)
-const bgImg = document.createElement("img")
+const bgImg = document.querySelector("#bgImg")
 bgImg.src = `img/${selected}.jpg`
 document.body.appendChild(bgImg)
